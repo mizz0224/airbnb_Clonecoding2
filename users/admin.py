7fb25bb0a84e2b5272e5e5561d4eac4115065ec5
mizz0224 <wosud0224@naver.com>
2020-12-31 18:17:44 +0900

    3.5

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
-@admin.register(models.User)
+
+@admin.register(
+    models.User
+)  # CustomUserAdmin 클래스의 아래부분에 admin.site.register(models.User,CustomUserAdmin) 을 작성한것과 같은기능을함
 class CustomUserAdmin(admin.ModelAdmin):
     pass
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
