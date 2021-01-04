0dfc491ea0978b39ad7af2f1d420070622cbfcb8
mizz0224 <wosud0224@naver.com>
2021-01-04 15:17:13 +0900

    7.3

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,14 +7,34 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-
     """ Room Admin Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
     list_display = (
         "name",
         "country",
@@ -27,10 +47,17 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
         "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
         "city",
         "country",
     )
@@ -38,14 +65,17 @@ class RoomAdmin(admin.ModelAdmin):
         "=city",
         "^host__username",
     )  # xaaa : x가 ^ <- aaa로 시작 , = <- aaa와 완전히일치(대소문자 구별x), @ <- aaa , none(아무것도 없음) <- icontatins(포함)
+    filter_horizontal = ("amenities", "facilities", "house_rules")
     # fields = "country" : admin에서  country만 표시
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
-    pass
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-
-    """ Photo Admin Definition """
+    """ """
 
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,7 @@ from core import models as core_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
+
     """ Abstract Item """
 
     name = models.CharField(max_length=80)
@@ -33,7 +34,10 @@ class Amenity(AbstractItem):
 
 
 class Facility(AbstractItem):
+
     """ Facility Model Definition """
+
+    pass
 
     class Meta:
         verbose_name_plural = "Facilities"
@@ -53,13 +57,14 @@ class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
 
 
 class Room(core_models.TimeStampedModel):
+
     """ Room Model Definition """
 
     name = models.CharField(max_length=140)
@@ -75,11 +80,15 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField("Amenity", blank=True)
-    facilities = models.ManyToManyField("Facility", blank=True)
-    house_rules = models.ManyToManyField("HouseRule", blank=True)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):  # <- 파이썬에서모든 클래스는 str 클래스로 인식하기때문에  __str__ 메소드를 오버라이딩 할수있다
         return self.name
