80750b9ed5eb3cc073ac28fb36b1ed0cc6eb9870
mizz0224 <61971962+mizz0224@users.noreply.github.com>
2021-09-12 23:14:08 +0900

    23.6

--- rooms/views.py
+++ rooms/views.py
@@ -2,10 +2,12 @@ from django.http import Http404
 from django.views.generic import ListView, DetailView, View, UpdateView
 from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
+from django.views.generic.edit import CreateView, FormView
 from . import models, forms
 from django.contrib import messages
 from users import mixins as user_mixins
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class HomeView(ListView):
@@ -164,3 +166,31 @@ def delete_photo(request, room_pk, photo_pk):
         return redirect(reverse("rooms:photos", kwargs={"pk": room_pk}))
     except models.Room.DoesNotExist:
         return redirect(reverse("core:home"))
+
+
+class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
+    model = models.Photo
+    template_name = "rooms/photo_edit.html"
+    pk_url_kwarg = "photo_pk"
+    success_message = "Photo Updated"
+    fields = ("caption",)
+
+    def get_success_url(self):
+        room_pk = self.kwargs.get("room_pk")
+        return reverse("rooms:photos", kwargs={"pk": room_pk})
+
+
+class AddPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, FormView):
+    model = models.Photo
+    template_name = "rooms/photo_create.html"
+    fields = (
+        "file",
+        "caption",
+    )
+    form_class = forms.CreatePhotoForm
+
+    def form_valid(self, form):
+        pk = self.kwargs.get("pk")
+        form.save(pk)
+        messages.success(self.request, "Photo Uploaded")
+        return redirect(reverse("rooms:photos", kwargs={"pk": pk}))
