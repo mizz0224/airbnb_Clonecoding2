9efc768feaadc2a5d5c375eba4fe96cfd46411b3
mizz0224 <61971962+mizz0224@users.noreply.github.com>
2021-10-10 17:01:34 +0900

    24.14

--- reservations/views.py
+++ reservations/views.py
@@ -1,11 +1,11 @@
 import datetime
+from django.http import Http404
 from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from rooms import models as room_models
+from reviews import forms as review_forms
 from . import models
-import reservations
-from django.http import Http404, request
 
 
 class CreateError(Exception):
@@ -40,9 +40,11 @@ class ReservationDetailView(View):
             and reservation.room.host != self.request.user
         ):
             raise Http404()
-
+        form = review_forms.CreateReviewForm()
         return render(
-            self.request, "reservations/detail.html", {"reservation": reservation}
+            self.request,
+            "reservations/detail.html",
+            {"reservation": reservation, "form": form},
         )
 
 
